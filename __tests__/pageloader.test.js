ae4d96a272030c17c530a5f5643cdee34e15d135
Artkiller971 <koshevnikod@gmail.com>
2021-06-11 02:06:36 +0300

    implemented downloading assets feature

--- __tests__/pageloader.test.js
+++ __tests__/pageloader.test.js
@@ -8,29 +8,43 @@ import downloadPage from '../src/getPageHtml';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.resolve(__dirname, `__fixtures__/${filename}`);
+const getFixturePath = (filename) => path.join(__dirname, `__fixtures__/${filename}`);
+
+// const resources = {
+// img: { url: '/img/test.png', file: getFixturePath('img/nodejs.png'), contentType: 'image/png' },
+// };
 
 const testFile = getFixturePath('test.html');
 const expectedFile = getFixturePath('test_expected.html');
 
-const server = 'https://host';
-const page = '/test';
+const server = 'https://ru.hexlet.io';
+const page = '/';
 
 let outputDir;
+let imageAsset;
+let srcHtml;
+
+beforeAll(async () => {
+  imageAsset = await fs.readFile(getFixturePath('nodejs.png'));
+  srcHtml = await fs.readFile(testFile);
+
+  nock(server)
+    .get('/nodejs.png')
+    .reply(200, imageAsset);
+
+  nock(server)
+    .get(page)
+    .reply(200, srcHtml);
+});
 
 beforeEach(async () => {
   outputDir = await fs.mkdtemp(path.join(os.tmpdir(), path.sep));
 });
 
 test('downloadPage', async () => {
-  nock(server)
-    .get(page)
-    .replyWithFile(200, testFile, { 'Content-Type': 'text/html' });
-
-  const testUrl = `${server}${page}`;
-  await downloadPage(testUrl, outputDir);
+  await downloadPage(server, outputDir);
 
   const expected = await fs.readFile(expectedFile);
-  const actual = await fs.readFile(`${outputDir}/host-test.html`);
+  const actual = await fs.readFile(`${outputDir}/ru-hexlet-io.html`);
   expect(actual.toString()).toBe(expected.toString());
 });
--- src/getPageHtml.js
+++ src/getPageHtml.js
@@ -1,24 +1,81 @@
-import { promises as fs } from 'fs';
+import fs, { promises as fsp } from 'fs';
 import axios from 'axios';
 import path from 'path';
+import cheerio from 'cheerio';
+import debug from 'debug';
 
-const urlToFilename = (url) => {
-  const { hostname, pathname } = new URL(url);
-  const hostnameAndPathname = `${hostname}${pathname}`;
-  const result = hostnameAndPathname.replace(/\W/g, '-');
-  return `${result}.html`;
+const log = debug('page-loader');
+
+const urlToFilenameWithoutExt = (url) => {
+  const { host, pathname } = new URL(url);
+  const hostnameAndPathname = `${host}${pathname}`;
+  const filename = hostnameAndPathname.replace(/\W/g, '-').replace(/-$/, '');
+  return filename;
 };
 
-// const writeFile = (filepath, data) => fs.writeFile(filepath, data);
+const urlToFilenameWithExt = (url) => {
+  const { ext } = path.parse(url);
+  const urlWithoutExt = url.slice(0, url.length - ext.length);
+  const filename = urlToFilenameWithoutExt(urlWithoutExt);
+  const newExt = ext ?? '.html';
+  return `${filename}${newExt}`;
+};
 
 const downloadPageHtml = (pageUrl) => axios.get(pageUrl).then((response) => response.data);
 
-const checkDirAccess = (dir) => fs.access(dir);
+const downloadBinaryResource = (resourceUrl, filepath) => axios.get(resourceUrl, { responseType: 'stream' })
+  .then((response) => response.data.pipe(fs.createWriteStream(filepath)));
+
+const downloadTextResource = (resourceUrl, filepath) => axios.get(resourceUrl)
+  .then((response) => fsp.writeFile(filepath, response.data));
+
+const resourceTypes = {
+  img: { attr: 'src', download: downloadBinaryResource },
+  script: { attr: 'src', download: downloadTextResource },
+  link: { attr: 'href', download: downloadTextResource },
+};
+
+const processAllTags = (url, htmlString, resourceDir) => {
+  const assetUrls = [];
+  const $ = cheerio.load(htmlString, { decodeEntities: false });
+  const tags = Object.keys(resourceTypes);
+  tags.forEach((tag) => {
+    $(tag).each(function () {
+      const oldSrc = $(this).attr(resourceTypes[tag].attr);
+      if (oldSrc.startsWith('//')) {
+        log('absolute url, skipping');
+      } else {
+        const assetUrl = new URL(oldSrc, url).href;
+        const assetFilename = urlToFilenameWithExt(assetUrl);
+        const assetFilepath = path.join(resourceDir, assetFilename);
+        const assetDownloadFunction = resourceTypes[tag].download;
+        assetUrls.push({ assetUrl, assetFilepath, assetDownloadFunction });
+        console.log(assetFilepath);
+        $(this).attr(resourceTypes[tag].attr, assetFilepath);
+      }
+    });
+  });
+  return { assetUrls, html: $.html() };
+};
+
+const downloadResources = (assetUrls, outputDir) => {
+  assetUrls.forEach(({ assetUrl, assetFilepath, assetDownloadFunction }) => {
+    assetDownloadFunction(assetUrl, path.join(outputDir, assetFilepath));
+  });
+};
+const checkDirAccess = (dir) => fsp.access(dir);
 
 export default (url, outputDir) => {
-  const filepath = path.join(outputDir, urlToFilename(url));
+  const baseName = urlToFilenameWithoutExt(url);
+  const filename = `${baseName}.html`;
+  const resourceDir = `${baseName}_files`;
+  const destFilepath = path.join(outputDir, filename);
+  const resourcePath = path.join(outputDir, resourceDir);
   return checkDirAccess(outputDir)
+    .then(() => fsp.mkdir(resourcePath))
     .then(() => downloadPageHtml(url))
-    .then((data) => fs.writeFile(filepath, data))
-    .then(() => filepath);
+    .then((data) => processAllTags(url, data, resourceDir))
+    .then(({ assetUrls, html }) => fsp.writeFile(destFilepath, html).then(() => assetUrls))
+    .then((assetUrls) => downloadResources(assetUrls, outputDir))
+    .then(() => filename);
 };
