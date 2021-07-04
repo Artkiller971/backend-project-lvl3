65babf65c22336935ba51d3d09e680a150fc1a0c
Artkiller971 <koshevnikod@gmail.com>
2021-07-04 10:14:13 +0300

    added -o option to cli

--- src/bin/pageLoader.js
+++ src/bin/pageLoader.js
@@ -9,7 +9,7 @@ const getFullPath = (pathname = '') => path.resolve(process.cwd(), pathname);
 program
   .version('0.0.1', '-v, --version')
   .description('Page loader utility.')
-  .option('--output [dir]', 'output dir', process.cwd())
+  .option('-o, --output [dir]', 'output dir', process.cwd())
   .arguments('<url>')
   .action((url) => {
     const dest = getFullPath(program.opts().output);
