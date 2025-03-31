31f15564745dfee9cf80cea8ae7c12d216c30bf5
Anton <koshevnikod@gmail.com>
2025-03-31 17:49:08 +0300

    added default dir argument

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import * as fs from 'node:fs/promises';
 import debug from 'debug';
 import axiosDebug from 'axios-debug-log';
+import process from 'node:process';
 
 import download from './src/download.js';
 import getAssets from './src/getAssets.js';
@@ -9,7 +10,7 @@ import getAssets from './src/getAssets.js';
 const log = debug('page-loader');
 axiosDebug(axios);
 
-export default (url, outputDir) => {
+export default (url, outputDir = process.cwd()) => {
   log('Trying to access the directory');
 
   return fs.access(outputDir, fs.constants.R_OK | fs.constants.W_OK)
